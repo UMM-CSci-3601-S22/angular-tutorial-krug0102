fa2fc3b02d31372438496c2d65abf0dfff5ce447
Zeke Krug <krug0102@morris.umn.edu>
2022-02-11 16:33:29 -0600

    Make `getShippingPrices()` use relative path to `shipping.json`
    Changing this in an attempt to fix the problem with GH pages not displaying the shipping prices.  The angular tutorial says to use the absolute path, which might be the cause.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -25,6 +25,6 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+    return this.http.get<{type: string, price: number}[]>('./assets/shipping.json');
   }
 }
